975e08340fd91bc090c919f9675ad85a5b4c818c
sulabi <168682205+sulabi@users.noreply.github.com>
2026-04-19 05:19:22 +0100

    add python lsp quick

--- .config/nvim/lua/sulvim/plugins/lspconfig.lua
+++ .config/nvim/lua/sulvim/plugins/lspconfig.lua
@@ -202,6 +202,9 @@ return {
 			}
 		})
 		vim.lsp.enable("jdtls")
+
+		vim.lsp.enable("pyright")
+
 		-- TODO: finish lsp
 	end,
 }
--- .config/nvim/lua/sulvim/plugins/mason.lua
+++ .config/nvim/lua/sulvim/plugins/mason.lua
@@ -47,6 +47,7 @@ return {
                 -- "eslint",
                 "marksman",
 
+				"pyright",
 				"jdtls",
             },
         })
